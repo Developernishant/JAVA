cadbc96860a727379a9dcb3f247273e5b6001325
nishant singh rajput <nishantsinghrajput675@gmail.com>
2025-04-22 16:11:03 +0530

    first commit

---
+++ class1.java
@@ -0,0 +1,6 @@
+public class class1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a test of the class1.java file.");
+    }
+}
